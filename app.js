2c061d8db2089b0dea5e88243d40fd0001af44a7
Mauricio <canetemauricio@hotmail.com>
2020-09-17 20:19:59 -0300

    Agregando estilos de fuente, tamaño y color al index

--- app.js
+++ app.js
@@ -4,30 +4,20 @@ const path = require('path')
 
 app.get('/', function(req,res){
 
-let file= path.resolve('vistas/index.html')
+let file= path.resolve('public/vistas/index.html')
     res.sendFile(file)
 
 })
 
 
-app.get('*', function(req,res) {
+app.get('*', function(req,res){
 
-if(req.url.endsWith('css')){
-    let file = path.resolve('styles'+req.url)
-    return res.sendFile(file)
-}
-
-let images = ['jpg','jpeg','gif','ping','bmp','webp','svg']
-let ext = req.url.split('.')[1]
-
-if (images.includes(ext)){
-    let file=path.resolve('images' + req.url)
+if(req.url.includes('.')){
+    let file = path.resolve('public'+req.url)
     return res.sendFile(file)
 }
 
 })
-
-// Listen
 
 app.listen(3000, () =>{
     console.log('escuchando en puerto 3000')
